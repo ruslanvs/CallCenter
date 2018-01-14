08c41c39c4d6ea0978b1131ddd7b5918574463f5
Ruslan Suvorov <ruslansuvorov@Ruslans-MacBook-Pro.local>
2018-01-14 15:38:11 -0800

    Part 1 done

--- CallCenter.py
+++ CallCenter.py
@@ -1,3 +1,5 @@
+calls = []
+
 class Call( object ):
     def __init__ ( self, id = "-", callerName = "-", callerPhnNumber = "-", timeOfCall = "-", reasonForCall = "-" ):
         self.id = id
@@ -5,24 +7,57 @@ class Call( object ):
         self.callerPhnNumber = callerPhnNumber
         self.timeOfCall = timeOfCall
         self.reasonForCall = reasonForCall
-    def displayCalls( self, id ): # for a specific call or all calls?
+        calls.append( self )
+    def displayCalls( self ): # for a specific call or all calls?
         print self.id
         print self.callerName
         print self.callerPhnNumber
         print self.timeOfCall
-        print self.reasonForCall
+        print "reason: ", self.reasonForCall
+        return self
         
 
 class CallCenter( object ):
-    def __init__( self, *calls, queueSize ):
-        self.calls = Call
+    def __init__( self, callList = [], queueSize = 0 ):
+        # self.instancelist = [ Call() for i in range(2) ]
+        self.callList = callList
         self.queueSize = queueSize
-        print self.calls
-    def add( self ):
-        pass
+    def addCall( self, newCall ):
+        self.callList.append( newCall )
+        self.queueSize = len( self.callList )
+        # print "self.callist", self.callList
+        # print "queue size", self.queueSize
 
-call1 = Call( "1", "Peter", "123-456-7889", "wanted to talk to someone" )
-call2 = Call()
-call3 = Call()
 
-print Call
+call1 = Call( "1", "Peter", "123-456-7889", "2:34 pm",  "Wanted to talk to someone" )
+call2 = Call( "14", "Sam", "345-452-3452", "4:54 am", "Nothing important" )
+call3 = Call( "39", "Tom", "442-457-8254", "1:34 pm", "Life threatening emergency")
+call4 = Call( "57", "Anna", "487-381-5281", "5:72 pm", "Wanted a recepy of a pie")
+
+call1.displayCalls()
+call2.displayCalls()
+call3.displayCalls()
+
+
+valleyCallCenter = CallCenter()
+valleyCallCenter.addCall( call1 )
+valleyCallCenter.addCall( call2 )
+valleyCallCenter.addCall( call3 )
+valleyCallCenter.addCall( call4 )
+# print valleyCallCenter.callList[0].callerName
+
+
+
+# print valleyCallCenter.callList
+
+# calls[0].displayCalls()
+# print calls[0]
+# del calls[0]
+# print call1
+# print calls[0]
+# del call1
+# print call1
+
+# print call1["id"]
+# call1.displayCalls()
+
